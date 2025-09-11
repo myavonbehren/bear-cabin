741c0aaaeb9c36b3094d5a739aba94994070c268
Mya Von Behren <mvonbehr@depaul.edu>
2025-09-11 01:08:09 -0400

    Remove welcome text from Home component in index.tsx to streamline content presentation.

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,10 +9,7 @@ const Home: NextPage = () => {
         <meta name="description" content="Welcome to Bear Cabin - Your perfect nature retreat" />
       </Head>
       <div className="container mx-auto px-4 py-8">
-        <h1 className="text-4xl font-thin text-center">Welcome to Bear Cabin</h1>
-        <p className="text-center mt-4 text-gray-600">
-          Your perfect escape into nature
-        </p>
+          
       </div>
     </>
   );
