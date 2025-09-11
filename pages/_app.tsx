8695358c1cf901e57b2297c5373346678298a797
Mya Von Behren <mvonbehr@depaul.edu>
2025-09-11 00:36:30 -0400

    Integrate NavBar component into the main application layout, enhancing the overall structure with a responsive design.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import type { AppProps } from "next/app";
-import "../styles/index.css";
+import type { AppProps } from 'next/app';
+import NavBar from '../components/NavBar';
+import '../styles/index.css';
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <NavBar />
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </div>
+  );
 }
